f2010d825240ab909501acee857b4202b1bb0733
navan93 <navan93@gmail.com>
2015-02-18 22:12:02 +0530

    lidar values fix

--- lidar_serial.py
+++ lidar_serial.py
@@ -7,38 +7,9 @@ port= serial.Serial('/dev/ttyACM0',baudrate=115200,timeout=0.5)
 port.close()
 port.open()
 
-md='\x4d\x44'
-start_step='\x30\x31\x32\x37'
-end_step='\x30\x31\x32\x39'
-cluster='\x30\x31'
-scan_int='\x30'
-num_scans='\x30\x31'
-lf='\x0a'
-
-data=md+start_step+end_step+cluster+scan_int+num_scans+lf
-
-print data
-
-port.write(data)
-
-resp=port.read(21)
-print binascii.b2a_hex(resp)
-
-resp=port.read(38)
-print binascii.b2a_hex(resp)
-resp_data=binascii.hexlify(resp[26:35])
-
-a= list(struct.unpack('38B',resp))
-b= [48]*9
-res= map(sub,a[26:35],b)
-print res
-length=len(res)/3
-dists=[0]*length
-for i in range(len(res)/3):
-	dists[i]+=res[i*3+0]*1000+res[i*3+1]*100+res[i*3+2]
-print dists
 
 def getscan(start,end,cluster):
+
 	start_step = "%04d"%start
 	end_step   = "%04d"%end
 	cluster    = "%02d"%cluster
@@ -50,7 +21,7 @@ def getscan(start,end,cluster):
 	port.write(data)
 
 	resp1 = port.read(21)
-	print binascii.b2a_hex(resp1)
+	#print binascii.b2a_hex(resp1)
 
 	steps 	 = end-start+1
 	data_len = steps*3
@@ -65,22 +36,22 @@ def getscan(start,end,cluster):
 		final_data_len = 27 + 66*quo + rem + 2
 
 	resp2 = port.read(final_data_len)
-	lines=resp2.splitlines()
-	del lines[0:4]
-	del lines[-1]
+	lines=resp2[26:-1].splitlines()
 	a = list()
 	for line in lines:
 		del line[-1]
 		a+= list(struct.unpack(str(len(line))+'B',line))
-	dec = [48]*data_len
+	dec = [48]*len(a)
 	res=map(sub,a,dec)
-	res_dists = map(sub,resp2,dec)
+	
 	print res
 	length=len(res)/3
 	dists=[0]*length
-	for i in range(len(res)/3):
-		dists[i]+=res[i*3+0]*1000+res[i*3+1]*100+res[i*3+2]
+	for i in range(length):
+		dists[i]=int(bin(res[i*3+0])[2:].zfill(6)+bin(res[i*3+1])[2:].zfill(6)+bin(res[i*3+2])[2:].zfill(6),2)
 	print dists
+getscan(44,725,1)
+port.close()
 
 
 
